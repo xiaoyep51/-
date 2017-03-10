20bae31975d74e46075bd8fc97add50d5a0c231e
xiaoyep51 <xiaoyep51@gmail.com>
2017-03-10 17:32:24 +0800

    -

---
+++ helper/SerializeHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RCApp_Win.Logic.Utility
+{
+    public class SerializeHelper
+    {
+        public static string SerializeObjectToJson<T>(T t)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Serialize(t);
+        }
+
+        public static string SerializeObjectToXml<T>(T t)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+
+            using (MemoryStream mem = new MemoryStream())
+            using (XmlTextWriter writer = new XmlTextWriter(mem, Encoding.UTF8))
+            {
+                //ns.Add("", "");
+                serializer.Serialize(writer, t, ns);
+                return Encoding.UTF8.GetString(mem.ToArray());
+            }
+        }
+
+        public static T DeserializeJsonToObject<T>(string json)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<T>(json);
+        }
+
+        public static T DeserializeXmlToObject<T>(string xml)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            try
+            {
+                xdoc.LoadXml(xml);
+                XmlNodeReader reader = new XmlNodeReader(xdoc.DocumentElement);
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                object obj = ser.Deserialize(reader);
+                return (T)obj;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public static DataTable JSONStringToDataTable<T>(string json)
+        {
+            DataTable dt = new DataTable();
+            if (json.IndexOf("[") > -1)//如果大于则strJson存放了多个model对象
+            {
+                json = json.Remove(json.Length - 1, 1).Remove(0, 1).Replace("},{", "};{");
+            }
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string[] items = json.Split(';');
+
+            foreach (PropertyInfo property in typeof(T).GetProperties())//通过反射获得T类型的所有属性
+            {
+                DataColumn col = new DataColumn(property.Name, property.PropertyType);
+                dt.Columns.Add(col);
+            }
+            //循环 一个一个的反序列化
+            for (int i = 0; i < items.Length; i++)
+            {
+                DataRow dr = dt.NewRow();
+                //反序列化为一个T类型对象
+                T temp = serializer.Deserialize<T>(items[i]);
+                foreach (PropertyInfo property in typeof(T).GetProperties())
+                {
+                    dr[property.Name] = property.GetValue(temp, null);
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+    }
+}
---
+++ helper/UtilityTool.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace RCApp_Win.Logic.Utility
+{
+    public class UtilityTool
+    {
+        /// <summary>
+        /// 深拷贝
+        /// 
+        /// Almost flawless. But as it was in my case, 
+        /// you have arrays of objects outside either mscorlib or the current assembly, 
+        /// you will need to use 
+        /// elementType = Type.GetType(type.AssemblyQualifiedName.Replace("[]&quo‌​t;, string.Empty));
+        /// </summary>
+        /// <param name="obj">待拷贝的对象</param>
+        /// <returns></returns>
+        public static object DeepCopy(object obj)
+        {
+            if (obj == null)
+                return null;
+            Type type = obj.GetType();
+
+            if (type.IsValueType || type == typeof(string))
+            {
+                return obj;
+            }
+            else if (type.IsArray)
+            {
+                Type elementType = Type.GetType(
+                     type.FullName.Replace("[]", string.Empty));
+                var array = obj as Array;
+                Array copied = Array.CreateInstance(elementType, array.Length);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    copied.SetValue(DeepCopy(array.GetValue(i)), i);
+                }
+                return Convert.ChangeType(copied, obj.GetType());
+            }
+            else if (type.IsClass)
+            {
+
+                object toret = Activator.CreateInstance(obj.GetType());
+                FieldInfo[] fields = type.GetFields(BindingFlags.Public |
+                            BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (FieldInfo field in fields)
+                {
+                    object fieldValue = field.GetValue(obj);
+                    if (fieldValue == null)
+                        continue;
+                    field.SetValue(toret, DeepCopy(fieldValue));
+                }
+                return toret;
+            }
+            else
+            {
+                throw new ArgumentException("Unknown type");
+            }
+        }
+    }
+}
